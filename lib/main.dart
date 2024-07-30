452bf5576c92a6cf80b693cdd264763ec85cf58e
David Tawfik <david.tawfik.intern@linkdevelopment.com>
2024-07-30 22:06:13 +0300

    finished rotating animation

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_animation_2/tween_rotate.dart';
 import 'tween_container.dart';
 import 'tween_positioned.dart';
 import 'tween_opacity.dart';
@@ -48,13 +49,12 @@ class _TweenAnimationExampleState extends State<TweenAnimationExample> {
             onPressed: () {
               Navigator.push(
                 context,
-                MaterialPageRoute(
-                    builder: (context) => TweenContainer()),
+                MaterialPageRoute(builder: (context) => TweenContainer()),
               );
             },
             child: const Text('Change Color'),
           ),
-                    const SizedBox(height: 20),
+          const SizedBox(height: 20),
           ElevatedButton(
             onPressed: () {
               Navigator.push(
@@ -65,16 +65,25 @@ class _TweenAnimationExampleState extends State<TweenAnimationExample> {
             },
             child: const Text('Change Position'),
           ),
-                              const SizedBox(height: 20),
+          const SizedBox(height: 20),
           ElevatedButton(
             onPressed: () {
               Navigator.push(
                 context,
-                MaterialPageRoute(
-                    builder: (context) => const TweenScale()),
+                MaterialPageRoute(builder: (context) => const TweenScale()),
               );
             },
             child: const Text('Change Scale'),
+          ),
+          const SizedBox(height: 20),
+          ElevatedButton(
+            onPressed: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => const TweenRotation()),
+              );
+            },
+            child: const Text('Rotate'),
           ),
         ],
       ),
